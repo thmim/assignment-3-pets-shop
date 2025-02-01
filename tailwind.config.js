906255b439ce3dca0afa1f407e87d0f60d3f49eb
thmim <mdtaharimmim1010@gmail.com>
2025-02-01 19:47:31 +0600

    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
+        animation: {
+          'custom-ping': 'customPing 1.5s cubic-bezier(0, 0, 0.2, 1) infinite',
+        },
+        keyframes: {
+          customPing: {
+            '0%': { transform: 'scale(1)', opacity: '1' },
+            '75%': { transform: 'scale(1.5)', opacity: '0' },
+            '100%': { transform: 'scale(2)', opacity: '0' },
+          },
+        },
         
       },
     },
